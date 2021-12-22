b6e3a9892a82861a9743ecc9d25aff41118f7aa6
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2021-12-22 22:18:14 +0200

    check_operand2

--- Game/Ex.asm
+++ Game/Ex.asm
@@ -1,18 +1,23 @@
 ;; Hany-> command;
 ;; Mena -> second; 
 
-;EXTRN mainGame:_AX, _BX, _CX, _DX, _SI, _DI, _SP, _BP
-;EXTRN mainGame:command
-;EXTRN mainGame:_01,_02,_03,_04,_05,_06,_07,_08,_09,_A,_B,_C,_D,_E,_F
-;PUBLIC result
+EXTRN mainGame:_AX, _BX, _CX, _DX, _SI, _DI, _SP, _BP,_AL, _BL, _CL, _DL, _AH, _BH, _CH, _DH
+EXTRN mainGame:command
+EXTRN mainGame:_01,_02,_03,_04,_05,_06,_07,_08,_09,_A,_B,_C,_D,_E,_F
+PUBLIC result
 
 .MODEL SMALL
 .STACK 64
 .DATA
 
 command_splited db 5 dup('$') 
-
+Operand1 db 5 dup('$')
+Operand2 db 5 dup('$')
+Two_Operands_Together_splited db 10 dup('$') 
+Operand2_Value db ?
+               
 HASH DW ?
+HASH_Operand2 DW ?
 
 command DB 'MUL AX,13'
 
@@ -27,7 +32,7 @@ ENDM DisplayString
 
 ; Hany
 ;; HASHING
-HASHING MACRO STR
+HASHING MACRO STR HASH
             
     mov SI,offset STR
     mov DI, offset HASH
@@ -66,10 +71,13 @@ MAIN PROC far
 	mov es,ax
 	  
     CALL split_command
+    split_operands Two_Operands_Together_splited Operand1 Operand2
     
-    HASHING command_splited
+    HASHING command_splited HASH
+    HASHING command_splited HASH_Operand2
     
-    CALL check_command                 
+    CALL check_command
+    CALL check_Operand2                 
      
     HLT
 	     
@@ -77,7 +85,8 @@ MAIN ENDP
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Hany
 
-;; split operand and store it in split_command
+;; split operand and store it in split_command 
+;; split operands and store it in Two_Operands_Together_splited
 split_command               PROC
     mov SI, offset command
     mov DI, offset command_splited
@@ -86,6 +95,13 @@ moving1:
 	MOVSB
 	cmp al,[SI]
 	jnz moving1
+;; Mena	
+	mov DI, offset Two_Operands_Together_splited
+	mov al,'$' ;; to check end
+moving11:
+    MOVSB
+    cmp al,[SI]
+    jnz moving11	
 
     ret
 
@@ -266,7 +282,487 @@ check_command                ENDP
 END MAIN   
 
 
+;;Mena
 
+check_Operand2                PROC 
+
+    MOV SI,offset HASH_Operand2 
+    MOV DI,offset Operand2
+     
+    ;; AX
+    
+    CMP [SI],2ocH
+    JNZ CHECKBX
+    
+    mov ax,_AX;; CODE
+    DisplayString Operand2 
+    
+    CHECKBX:
+    
+    
+    ;; BX
+    
+    CMP [SI],210H
+     JNZ CHECKCX
+    
+    mov ax,_BX;; CODE
+    DisplayString Operand2
+    
+    CHECKCX:
+    
+    ;; CX 
+    
+    CMP [SI],214H
+     JNZ CHECKDX
+    
+    mov ax,_CX;; CODE                    
+    DisplayString Operand2
+    
+    CHECKDX:
+    
+    ;; DX   
+    
+    CMP [SI],218H
+     JNZ CHECKSI
+    
+    mov ax,_DX;; CODE
+    DisplayString Operand2
+    
+    CHECKSI:
+    
+    ;; SI   
+    
+    CMP [SI],227H
+     JNZ CHECKDI
+    
+    mov ax,_SI;; CODE
+    DisplayString Operand2
+    
+    CHECKDI:
+    
+    ;; DI   
+    
+    CMP [SI],1ebH
+     JNZ CHECKSP
+    
+    mov ax,_DI;; CODE
+    DisplayString Operand2
+    
+    CHECKSP:
+    
+    ;; SP   
+    
+    CMP [SI],23cH
+     JNZ CHECKBP
+    
+    mov ax,_SP;; CODE
+    DisplayString Operand2
+    
+    CHECKBP:
+    
+    ;; BP   
+    
+    CMP [SI],1f8H
+     JNZ CHECKAL
+    
+    mov ax,_BP;; CODE
+    DisplayString Operand2
+    
+    CHECKAL:
+    
+    ;; AL   
+    
+    CMP [SI],1e8H
+     JNZ CHECKBL
+    CMP Operand2,'A'
+     JNZ CHECKDH 
+    
+    mov ax,_AL;; CODE
+    DisplayString Operand2
+    
+    CHECKBL:
+    
+    ;; BL    
+    
+    CMP [SI],1ecH
+     JNZ CHECKCL
+    
+    mov ax,_BL;; CODE
+    DisplayString Operand2
+    
+    CHECKCL:
+    
+    ;; CL   
+    
+    CMP [SI],1f0H
+     JNZ CHECKDL
+   
+    mov ax,_CL;; CODE
+    DisplayString Operand2
+    
+    CHECKDL:
+    
+    ;; DL   
+    
+    CMP [SI],1f4H      
+     JNZ CHECKAH
+    
+    mov ax,_DL;; CODE
+    DisplayString Operand2
+    
+    CHECKAH:
+    
+    
+    ;; AH   
+    
+    CMP [SI],1dcH
+     JNZ CHECKBH
+    
+    mov ax,_AH;; CODE
+    DisplayString Operand2
+    
+    CHECKBH:
+    
+    ;; BH   
+    
+    CMP [SI],1e0H
+     JNZ CHECKCH
+    
+    mov ax,_BH;; CODE
+    DisplayString Operand2
+    
+    CHECKCH:
+    
+    ;; CH   
+    
+    CMP [SI],1e4H
+     JNZ CHECKDH
+    
+    mov ax,_CH;; CODE
+    DisplayString Operand2
+    
+    CHECKDH:
+    
+    ;; DH  
+    
+    CMP [SI],1e8H 
+     JNZ CHECK01
+     
+    mov ax,_DH;; CODE
+    DisplayString Operand2
+    
+    CHECK01:
+    
+    ;; 01  
+    
+    CMP [SI],3a6H 
+    JNZ CHECK02
+     movml1:
+    mov ax,_01;; CODE 
+ DisplayString Operand2
+    
+    CHECK02:
+    
+    ;; 02  
+    
+    CMP [SI],3a9H 
+     JNZ CHECK03
+      movml2:
+    mov ax,_02;; CODE 
+ DisplayString Operand2 
+    
+    CHECK03:
+    
+    ;; 03  
+    
+    CMP [SI],3acH 
+     JNZ CHECK04
+      movml3:
+    mov ax,_03;; CODE 
+ DisplayString Operand2
+    
+    CHECK04:
+    
+    ;; 04  
+    
+    CMP [SI],3afH 
+     JNZ CHECK05
+       movml4:
+    mov ax,_04;; CODE 
+ DisplayString Operand2    
+    CHECK05:
+    
+    ;; 05  
+    
+    CMP [SI],3b2H 
+     JNZ CHECK06
+      movml5:
+    mov ax,_05;; CODE 
+ DisplayString Operand2    
+    CHECK06:
+    
+    ;; 06  
+    
+    CMP [SI],3b5H 
+     JNZ CHECK07
+      movml6:
+    mov ax,_06;; CODE 
+ DisplayString Operand2    
+    CHECK07:
+    
+    ;; 07  
+    
+    CMP [SI],3b8H 
+     JNZ CHECK08
+      movml7:
+    mov ax,_07;; CODE 
+ DisplayString Operand2    
+    CHECK08:
+    
+    ;; 08  
+    
+    CMP [SI],3bbH 
+     JNZ CHECK09
+      movml8:
+    mov ax,_08;; CODE 
+ DisplayString Operand2       
+    CHECK09:
+    
+    ;; 09  
+    
+    CMP [SI],3beH 
+     JNZ CHECKA
+      movml9:
+    mov ax,_09;; CODE 
+ DisplayString Operand2    
+    CHECKA:
+    
+    ;; A  
+    
+    CMP [SI],346H 
+     JNZ CHECKB
+      movmlA:
+    mov ax,_A;; CODE 
+ DisplayString Operand2     
+    CHECKB:
+    
+    ;; B  
+    
+    CMP [SI],349H 
+     JNZ CHECKC
+      movmlB:
+    mov ax,_B;; CODE 
+ DisplayString Operand2     
+    CHECKC:
+    
+    ;; C  
+    
+    CMP [SI],34cH 
+     JNZ CHECKD
+      movmlC:
+    mov ax,_C;; CODE 
+ DisplayString Operand2     
+    CHECKD:
+    
+    ;; D  
+    
+    CMP [SI],34fH 
+     JNZ CHECKE
+      movmlD:
+    mov ax,_D;; CODE 
+ DisplayString Operand2     
+    CHECKE:
+    
+    ;; E  
+    
+    CMP [SI],352H 
+     JNZ CHECKF
+      movmlE:
+    mov ax,_E;; CODE 
+ DisplayString Operand2         
+    CHECKF:
+    
+    ;; F  
+    
+    CMP [SI],355H 
+     JNZ CHECKmlSI 
+      movmlF:
+    mov ax,_F;; CODE 
+ DisplayString Operand2          
+    CHECKmlSI:
+    
+    ;; [SI] 
+    
+    CMP [SI],457H 
+    JNZ CHECKmlDI
+            
+             
+    cmp _SI,1
+    jz movml1
+    cmp _SI,2
+    jz movml2
+    cmp _SI,3
+    jz movml3
+    cmp _SI,4
+    jz movml4
+    cmp _SI,5
+    jz movml5
+    cmp _SI,6
+    jz movml6
+    cmp _SI,7
+    jz movml7
+    cmp _SI,8
+    jz movml8
+    cmp _SI,9
+    jz movml9
+    cmp _SI,A
+    jz movmlA
+    cmp _SI,B
+    jz movmlB
+    cmp _SI,C
+    jz movmlC
+    cmp _SI,D
+    jz movmlD
+    cmp _SI,E
+    jz movmlE
+    cmp _SI,F
+    jz movmlF
+    ;; CODE 
+ DisplayString Operand2              
+    CHECKmlDI:
+    
+    ;; [DI] 
+    
+    CMP [SI],42aH 
+    JNZ CHECKmlSP
+    
+    cmp _DI,1
+    jz movml1
+    cmp _DI,2
+    jz movml2
+    cmp _DI,3
+    jz movml3
+    cmp _DI,4
+    jz movml4
+    cmp _DI,5
+    jz movml5
+    cmp _DI,6
+    jz movml6
+    cmp _DI,7
+    jz movml7
+    cmp _DI,8
+    jz movml8
+    cmp _DI,9
+    jz movml9
+    cmp _DI,A
+    jz movmlA
+    cmp _DI,B
+    jz movmlB
+    cmp _DI,C
+    jz movmlC
+    cmp _DI,D
+    jz movmlD
+    cmp _DI,E
+    jz movmlE
+    cmp _DI,F
+    jz movmlF 
+    ;; CODE 
+ DisplayString Operand2               
+    CHECKmlSP:
+    
+    ;; [SP] 
+    
+    CMP [SI],46cH 
+     JNZ CHECKmlBP
+    
+    cmp _SP,1
+    jz movml1
+    cmp _SP,2
+    jz movml2
+    cmp _SP,3
+    jz movml3
+    cmp _SP,4
+    jz movml4
+    cmp _SP,5
+    jz movml5
+    cmp _SP,6
+    jz movml6
+    cmp _SP,7
+    jz movml7
+    cmp _SP,8
+    jz movml8
+    cmp _SP,9
+    jz movml9
+    cmp _SP,A
+    jz movmlA
+    cmp _SP,B
+    jz movmlB
+    cmp _SP,C
+    jz movmlC
+    cmp _SP,D
+    jz movmlD
+    cmp _SP,E
+    jz movmlE
+    cmp _SP,F
+    jz movmlF
+    ;; CODE 
+ DisplayString Operand2    
+                   
+    CHECKmlBP:
+    
+    ;; [BP] 
+    
+    CMP [SI],439H 
+     JNZ CHECKdata
+    
+    cmp _BP,1
+    jz movml1
+    cmp _BP,2
+    jz movml2
+    cmp _BP,3
+    jz movml3
+    cmp _BP,4
+    jz movml4
+    cmp _BP,5
+    jz movml5
+    cmp _BP,6
+    jz movml6
+    cmp _BP,7
+    jz movml7
+    cmp _BP,8
+    jz movml8
+    cmp _BP,9
+    jz movml9
+    cmp _BP,A
+    jz movmlA
+    cmp _BP,B
+    jz movmlB
+    cmp _BP,C
+    jz movmlC
+    cmp _BP,D
+    jz movmlD
+    cmp _BP,E
+    jz movmlE
+    cmp _BP,F
+    jz movmlF
+    ;; CODE 
+ DisplayString Operand2                                 
+                   
+    CHECKdata:
+    
+    ;; data 
+    
+     
+    JNZ end
+    
+    ;; CODE 
+ DisplayString Operand2
+ 
+    
+            
+    RET
+
+check_command                ENDP
 
                                                     
                                                     
