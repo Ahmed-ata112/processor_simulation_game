f3bf1b70501f563a083b9c9e718c158c4ac80feb
Ahmed-H300 <ahmed300hany@gmail.com>
2022-01-06 15:58:20 +0200

    chatting semi final

--- Game/Chatting.asm
+++ Game/Chatting.asm
@@ -3,12 +3,27 @@ mov ah,2
 mov dl,str
 int 21h
 endm Displaychar
+
+set MACRO ; work on dx ; L-> X  H->Y
+mov bh,0h
+mov ah,2
+int 10h
+    
+ENDM set
+
+get  MACRO 
+mov ah,3h
+mov bh,0h
+int 10h 
+ENDM get
+
+
 include MACR.INC
 .MODEL large
 .STACK 64
 .DATA
 
-
+nl db 10,13,'$'
 _LINE DB '-'
 _sign DB ':'
 VALUE db 'A'
@@ -38,19 +53,6 @@ y2 db 0CH
 
 
 .code
-
-set MACRO ; work on dx ; L-> X  H->Y
-mov bh,0h
-mov ah,2
-int 10h
-    
-ENDM set
-
-get  MACRO 
-mov ah,3h
-mov bh,0h
-int 10h 
-ENDM get
 
 Draw_chat proc
 
@@ -129,157 +131,48 @@ AGAIN:  	In al , dx
 ret
 send endp
 
-
-; receiver proc near
+receiver proc near
     
-;     mov dx , 3FDH
-;     CHK: in al , dx
-;     test al , 1
-;     JZ loo 
+    mov dx , 3FDH
+    CHK: in al , dx
+    test al , 1
+    JnZ loo
+    jmp FinishedrecChar_mess
+    loo: 
 
-;     mov dx , 03F8H
-;     in al , dx
-;     mov VALUE , al
-;     ret
-;     receiver endp
+    mov dx , 03F8H
+    in al , dx
+    mov VALUE , al
+    ret
+    receiver endp
 
-main proc far
-MOV AX,@DATA
-	MOV DS,AX
-	mov es,ax
-
-call intialize
-MOV Flag_exit,0H
-CLR_Screen_with_Scrolling_TEXT_MODE
-call Draw_chat
-
-chat_loop:
-
-
-
-
-
-
-jmp chat_loop
-
-
-; mov dh,y1
-; mov dl,x1
-; set
-
-;  mov ah,6       
-;  mov al,1            
-;  mov bh,7                
-;  mov ch,0       
-;  mov cl,0        
-;  mov dh,12     
-;  mov dl,79      
-;  int 10h
-
-; loo:
-
-; mov ah,1
-; int 16h
-; jz receive
-; mov ah,0
-; int 16h
-
-; mov VALUE,al
-; mov dh,y1
-; mov dl,x1
-; set
-; call send
-
-
-
-; mov dl,VALUE
-; mov ah,2
-; int 21h
-
-; get
-; mov x1,dl
-; mov y1,dh
-
-
-; cmp y1,12
-;     jz scroll_up_send
-;     jmp receive
-;    scroll_up_send:       
-;     mov ah,6 ; function 6
-;     mov al,1 ; scroll by 1 line
-;     mov bh,7 ; normal video attribute
-;     mov ch,00 ; upper left Y
-;     mov cl,00 ; upper left X
-;     mov dh,12 ; lower right Y
-;     mov dl,79 ; lower right X
-;     int 10h
-;     mov dh,11
-;     mov dl,0
-;     set
-;     jmp here
-
-; receive:
-
-
-
-; mov dh,y2
-; mov dl,x2
-; set
-; here:
-; call receiver
-
-; mov dl,VALUE
-; mov ah,2
-; int 21h
-
-
-; get
-; mov x2,dl
-; mov y2,dh
-
-; cmp y2,24
-;     jz scroll_up_receive
-;     jmp loo
-;    scroll_up_receive:       
-;     mov ah,6 ; function 6
-;     mov al,1 ; scroll by 1 line
-;     mov bh,7 ; normal video attribute
-;     mov ch,12 ; upper left Y
-;     mov cl,0 ; upper left X
-;     mov dh,24 ; lower right Y
-;     mov dl,79 ; lower right X
-;     int 10h
-;     mov dh,23
-;     mov dl,0
-
-;     set
-
-
-; jmp loo
-
-
-           
-
-
-
-
-
-
-main endp
-
-GetMess PROC
+Mess PROC
 mov ah,1
 int 16h ;-> looks at the buffer
-jz FinishedTakingChar ;nothing is clicked
+jnz out_range
+jmp FinishedTakingChar_mess ;nothing is clicked
+out_range:
+    mov dh,y1
+    mov dl,x1
+    set
 
-    mov ah,0
+    mov ah,0 ; get the char from the buffer
     int 16h
 
     ;check for enter
     cmp ah,28
     jne not_finished_yet ;THE_messege_sent IS THE DATA TO BE SENT
-
-    MOV CX,actualmessege_sent_Size
+    ; for new line
+    DisplayString nl
+    get
+    mov x1,dl
+    mov y1,dh
+    CMP actualmessege_sent_Size,0
+    JNE EMPTY_MESS
+    JMP FinishedTakingChar_mess
+    EMPTY_MESS:
+    xor ch,ch
+    MOV Cl,actualmessege_sent_Size
     MOV SI,OFFSET THE_messege_sent
     SENT_LOOP:
     MOV AL,[SI]
@@ -292,9 +185,10 @@ jz FinishedTakingChar ;nothing is clicked
     MOV DI, offset THE_messege_sent
     MOV CX,140
     REP STOSB
-
-
-    not_finished_yet
+    mov actualmessege_sent_Size,0
+    jmp FinishedTakingChar_mess
+    
+    not_finished_yet:
 
     ; CHECK_IF_ENTER F3 TO EXIT:
     cmp ah,61 ;; check if F3 is pressed
@@ -304,65 +198,160 @@ jz FinishedTakingChar ;nothing is clicked
     MOV AL,'$'
     MOV DI, offset THE_messege_sent
     MOV CX,140
+    mov actualmessege_sent_Size,0
     REP STOSB
 
-    jmp FinishedTakingChar
+    jmp FinishedTakingChar_mess
     NO_EXIT:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; mov dh,y1
-    ; mov dl,x1
-    ; set
 
-    ; cmp al,20H  ;;a space 
-    ; jb CHECK_IF_ENTER11 
-    ; cmp al, ']'
-    ; ja CHECK_IF_ENTER11
-    ; JMP ADD_TO_COMMAND   ;;its a valid one 
-
-
-    CHECK_IF_F2IS_PRESSED:
-    cmp ah,60 ;; check if F2 is pressed
-    jne CHECK_IF_BACKSLASH11
-    mov finished_taking_input,1
-    CMP GAME_LEVEL, 2
-    JNE ADD_TO_COMMAND
-    MOV EXECUTE_REVESED, 1
-    jmp ADD_TO_COMMAND  ;; TO ADD THE ENTER
-
-    CHECK_IF_BACKSLASH11:
     cmp ah,0eh      ;;backk
-    jne FinishedTakingChar  ;;NOT ANY OFTHE THREE CASES
+    JE ADD_TO_THE_messege_sent_ERR
+    jMP ADD_TO_THE_messege_sent  ;;NOT ANY OFTHE THREE CASES
+    ADD_TO_THE_messege_sent_ERR:
+    CMP actualmessege_sent_Size,0
+    JNE EMPTY_MESS1
+    JMP FinishedTakingChar_mess
+    EMPTY_MESS1:
     ;if size>0 then delete the last char and dec string
-    READ_KEY
-    cmp actualcommand_Size,0
-    je FinishedTakingChar
-    mov di,offset THE_COMMAND 
-    mov bl,actualcommand_Size 
+    ; READ_KEY
+    cmp actualmessege_sent_Size,0
+    JNE DEL_ERR
+    jMP FinishedTakingChar_mess
+    DEL_ERR:
+    mov di,offset THE_messege_sent 
+    mov bl,actualmessege_sent_Size 
     dec bl
     xor bh,bh
     add di,bx
     mov byte ptr [di], '$'   
-    dec actualcommand_Size
-    jmp FinishedTakingChar 
-    ADD_TO_COMMAND:
-    READ_KEY
+    dec actualmessege_sent_Size
+    cmp x1,0
+    je back_line
+    DEC x1
+    mov dh,y1
+    mov dl,x1
+    set
+    Displaychar ' '
+    ;over ride on space
+    mov dh,y1
+    mov dl,x1
+    set
+    jmp FinishedTakingChar_mess
+    back_line:
+    DEC y1
+    MOV X1,79
+    mov dh,y1
+    mov dl,x1
+    set
+    Displaychar ' '
+    ;over ride on space
+    mov dh,y1
+    mov dl,x1
+    set
+    jmp FinishedTakingChar_mess 
+    ADD_TO_THE_messege_sent:
+    ; READ_KEY
     ;then store The char and print it then inc the size
-   cmp actualcommand_Size,15  ;; command is full  -> in order to not delete $ at the end
-    je FinishedTakingChar
-    mov di,offset THE_COMMAND 
-    mov bl,actualcommand_Size
+    cmp actualmessege_sent_Size,139  ;; command is full  -> in order to not delete $ at the end
+    je FinishedTakingChar_mess
+    mov di,offset THE_messege_sent 
+    mov bl,actualmessege_sent_Size
     xor bh,bh
     add di,bx
     mov [di],al ;;the char is moved to the end of string
-    inc actualcommand_Size
-    FinishedTakingChar:
+    inc actualmessege_sent_Size
+    Displaychar al
+    ;; to get last position
+    get
+    mov x1,dl
+    mov y1,dh
+
+    FinishedTakingChar_mess:
+    ;; SEE IF I WANT TO SCROLL
+    CMP y1,0AH
+    JNE CONT_NO_SCROLL
+
+    mov ah,6       ; function 6
+    mov al,1        ; scroll by 1 line    
+    mov bh,7       ; normal video attribute         
+    mov ch,1       ; upper left Y
+    mov cl,0H        ; upper left X
+    mov dh,9     ; lower right Y
+    mov dl,79      ; lower right X 
+    int 10h           
+
+    DEC y1
+    MOV X1,0
+    mov dh,y1
+    mov dl,x1
+    set
+
+    CONT_NO_SCROLL:
+;;receiving
+
+    mov dh,y2
+    mov dl,x2
+    set
+    
+    call receiver
+
+    Displaychar VALUE
+
+    get
+    mov x2,dl
+    mov y2,dh
+
+    FinishedrecChar_mess:
+    ;; SEE IF I WANT TO SCROLL
+    CMP y1,14H
+    JNE CONT_NO_SCROLL1
+
+    mov ah,6       ; function 6
+    mov al,1        ; scroll by 1 line    
+    mov bh,7       ; normal video attribute         
+    mov ch,0BH       ; upper left Y
+    mov cl,0H        ; upper left X
+    mov dh,15H     ; lower right Y
+    mov dl,79      ; lower right X 
+    int 10h           
+
+    DEC y1
+    MOV X1,0
+    mov dh,y1
+    mov dl,x1
+    set
+
+    CONT_NO_SCROLL1:
+
+
     ret
-GetMess ENDP
+Mess ENDP
+
+
+main proc far
+MOV AX,@DATA
+	MOV DS,AX
+	mov es,ax ; for string operation
+
+call intialize
+MOV Flag_exit,0H
+CLR_Screen_with_Scrolling_TEXT_MODE
+call Draw_chat
+
+chat_loop:
+
+cmp Flag_exit,1
+jne cont_mess
+CLR_Screen_with_Scrolling_TEXT_MODE
+ret
+cont_mess:
 
 
 
+call Mess
 
-
+jmp chat_loop
+main endp
 end main
 
 
